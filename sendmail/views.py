1e2a327a4a477863b3b93a0fe1fa3af8d2752dd5
Naveen Kumar <bsainaveenkumar@gmail.com>
2021-05-04 12:26:19 +0530

    Update views.py

--- sendmail/views.py
+++ sendmail/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import smtplib
 from email.message import EmailMessage
+import pycountry
+from plotly.offline import plot
+import plotly.express as px
+import pandas as pd
 
 def instance():
     ec2s=["inst1","inst2","inst3"]
@@ -30,4 +34,9 @@ def my_def_in_view(request):
     return JsonResponse(data)
 def index(request):
     print("Current View: Index View")
-    return render(request, 'sendmail/index.html',{'ec2s': instance() })
+    URL_DATASET = r'https://raw.githubusercontent.com/datasets/covid-19/master/data/countries-aggregated.csv'
+    df1 = pd.read_csv(URL_DATASET)
+    df_india = df1[df1['Country'] == 'India']
+    fig = px.line(df_india, x='Date', y=['Recovered','Confirmed'])
+    plt_div = plot(fig, output_type='div')
+    return render(request, 'sendmail/index.html',{'plotly':plt_div})
